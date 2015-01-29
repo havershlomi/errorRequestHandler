444cbcbba12c62bd2a535299052dd29a2657d4cd
shlomihaver <shlomi@socialvyou.com>
2015-01-29 13:43:55 +0200

    add support in generalError Handler

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
 'use strict';
 
 var request = require("request"),
-    extend = require("extend");
-
-
+    extend = require("extend"),
+    async = require("async");
+//request options need to Accept json
+var defaults = {
+    requestOptions: {
+        headers: {
+            "Accept": "JSON",
+            "Content-Type": "application/json"
+        }, json: true
+    },
+    errorCodeField: "errorCode",
+    generalErrorHandler: function (response, body, next) {
+        next();
+    }
+};
 function errorBaseRequest(uri, options, errorObj, callback) {
-    this.defaults = {
-        requestOptions: {},
-        errorCodeField: "errorCode"
-    };
+    this.defaults = defaults;
     this.baseRequest = request.defaults({});
+    var self = this;
 
     return this.baseRequest(uri, options,
         function (err, response, body) {
             if (err) return callback(err);
+            async.series([function (next) {
+                self.generalErrorHandler(response, body, next);
+            }, function (next) {
+                var error = errorObj[body[self.options.errorCodeField]] || errorObj["*"];
 
+                if (error !== undefined) {
+                    if (error instanceof Error) return next(error);
+                    if (error instanceof Function) return error(response, body, function (err) {
+                        next(err, response, body);
+                    });
+                } else {
+                    return next();
+                }
 
-            var error = errorObj[body[this.options.errorCodeField]] || errorObj["*"];
-
-            if (error !== undefined) {
-                if (error instanceof Error) return callback(error);
-                if (error instanceof Function) return error(response, body, function (err) {
-                    callback(err, response, body);
-                });
-            }
-
-            return callback(new Error("Unknown Error", body[this.options.errorCodeField]));
+            }], function (err) {
+                if (err) return callback(err);
+                callback(null, response, body);
+            });
         });
 };
 
 errorBaseRequest.defaults = function (options) {
     extend(true, this.defaults, options);
-    if (options.errorCodeField === undefined || options.errorCodeField.length === 0) throw new Error("errorCodeField can't be empty or undefined");
+    if (options.errorCodeField !== undefined && (typeof options.errorCodeField !== "string" || options.errorCodeField.length === 0)) throw new Error("errorCodeField must be non empty string");
+    if (options.generalErrorHandler === undefined && !(options.generalErrorHandler instanceof Function)) throw new Error("generalErrorHandler must be a function");
     this.baseRequest = request.defaults(this.defaults.requestOptions || {});
 };
 
