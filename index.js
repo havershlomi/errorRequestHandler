16ecb21d37d280b6ed230b6aee11ab5cce642332
shlomihaver <shlomi@socialvyou.com>
2015-02-02 22:52:49 +0200

    fixed module and added example

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var request = require("request"),
-    extend = require("extend"),
-    baseRequest = require("./lib/baseRequest");
+var extend = require("extend"),
+    BaseRequest = require("./lib/baseRequest");
+
 //request options need to Accept json
 var defaults = {
     requestOptions: {
@@ -16,32 +16,18 @@ var defaults = {
         next();
     }
 };
+//Create base request object and run send the request
 function errorBaseRequest(uri, options, errorObj, callback) {
-    var defaults = {
-            requestOptions: {
-                headers: {
-                    "Accept": "JSON",
-                    "Content-Type": "application/json"
-                }, json: true
-            },
-            errorCodeField: "errorCode",
-            generalErrorHandler: function (response, body, next) {
-                next();
-            }
-        },
-        baseRequest = request.defaults(defaults.requestOptions);
-    var self = this;
-
-    return new baseRequest.BaseRequest(defaults);
+    console.log("here");
+    return new BaseRequest(defaults)(uri, options, errorObj, callback);
 };
+
+//update defaults and return the baseRequest
+module.exports = errorBaseRequest;
 
 errorBaseRequest.defaults = function (options) {
-    extend(true, this.defaults, options);
+    extend(true, defaults, options);
     if (options.errorCodeField !== undefined && (typeof options.errorCodeField !== "string" || options.errorCodeField.length === 0)) throw new Error("errorCodeField must be non empty string");
     if (options.generalErrorHandler === undefined && !(options.generalErrorHandler instanceof Function)) throw new Error("generalErrorHandler must be a function");
-    this.baseRequest = request.defaults(this.defaults.requestOptions || {});
+    return new BaseRequest(defaults);
 };
-
-
-
-module.exports = errorBaseRequest;
